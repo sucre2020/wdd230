8e184662a0015e3cd8276186173855f470e79884
sucre2020 <eff21004@byui.edu>
2022-07-03 01:12:42 +0100

    worked no navbar

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -71,3 +71,13 @@ setInterval(() =>{
     let d = new Date();
     time.innerHTML = d.toLocaleTimeString();
 },1000)
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
